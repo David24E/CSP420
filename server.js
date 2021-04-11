016d8fd8a9f43fbffb4a9d0e749162f4d2469c67
David <davidedeh24@gmail.com>
2021-04-11 21:27:46 +0100

    Emergency Fix pt6

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const socketToRoom = {};
 const rooms = {};
 const users = {};
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
