fbed5fc7bfcb54ac91a0fa70e9bff281254f6f56
David <davidedeh24@gmail.com>
2021-04-12 20:47:55 +0100

    Emergency Fix pt7

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { makeStyles } from '@material-ui/core/styles';
 import CreateRoom from './routes/CreateRoom';
